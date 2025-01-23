6575f1c35a05cfabc85c7771ed8ac3434445c7a4
ali-consciousroad <ali_dindar@live.be>
2025-01-23 19:44:04 +0000

    Mapp each candidate id to its respective candidate struc

--- lesson-1-writing-your-first-solidity-smart-contract/demos/prom-voting-system/starter/PromVoting.sol
+++ lesson-1-writing-your-first-solidity-smart-contract/demos/prom-voting-system/starter/PromVoting.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.19;
 
 contract PromVoting {
-    // Define a strcture to hold candidate details
+    // Define a struct to hold candidate details
     struct Candidate {
         uint id; // Unique identifier for the candidate 
         string name;
         uint voteCount;
     }   
+    // Mapping from candidate ID to candidate struct for storing candidates 
+    mapping(uint => Candidate) public candidates;
 }
