7cad626beceedc66eb939c1b929a8f3ee7a9df03
ali-consciousroad <ali_dindar@live.be>
2025-01-23 19:31:41 +0000

    Create the contract and a structure to hold candidate details

---
+++ lesson-1-writing-your-first-solidity-smart-contract/demos/prom-voting-system/starter/PromVoting.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.19;
+
+contract PromVoting {
+    // Define a strcture to hold candidate details
+    struct Candidate {
+        uint id; // Unique identifier for the candidate 
+        string name;
+        uint voteCount;
+    }   
+}
